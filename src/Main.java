83643c13ebf12a32655fea868e35fffb7f009dd3
pankajnema99 <pankajnema9@gmail.com>
2022-06-09 19:45:47 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -10,6 +10,9 @@ public class Main {
         System.out.println("Pankaj commit");
         System.out.println("Hello world!");
         System.out.println("Pankaj commit");
+        //Adding new code to master by another developer
+        System.out.println("Hello world! from developer 2");
+        System.out.println("Pankaj commit from developer 2");
 
     }
 }
