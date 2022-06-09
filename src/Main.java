2416efa3608028ede425f2163b0e50a282bee502
Pankaj Nema <pankaj.nema@collibra.com>
2022-06-09 19:47:30 +0200

    commit from developer 1

--- src/Main.java
+++ src/Main.java
@@ -10,6 +10,9 @@ public class Main {
         System.out.println("Pankaj commit");
         System.out.println("Hello world!");
         System.out.println("Pankaj commit");
+        //Adding new code to master by another developer1
+        System.out.println("Hello world! from developer 1");
+        System.out.println("Pankaj commit from developer1");
 
     }
 }
